6a6bc48f075145ca845efcf1911908c61344edec
Carlos <carlos.msv@hotmail.com>
2022-12-16 19:42:52 -0300

    testing if notification content throws errors if less than 5 or more than 240 characters

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,7 +1,15 @@
 import { Content } from "./content";
 
 test('it should be able to create a notification content', () => {
-    const content = new Content('You received a new notification');
+  const content = new Content('You received a new notification');
 
-    expect(content).toBeTruthy();
+  expect(content).toBeTruthy();
+});
+
+test('it should not be able to create a notification content with less than 5 characters', () => {
+  expect(() => new Content('You')).toThrow();
+});
+
+test('it should not be able to create a notification content with more than 240 characters', () => {
+  expect(() => new Content('a'.repeat(241))).toThrow();
 });
