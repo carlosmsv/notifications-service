688aa9943b428c131b758ac9cb3ad7eb6e8cd1fd
Carlos <carlos.msv@hotmail.com>
2022-12-17 06:14:03 -0300

    Added Cancel Notification Route

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,32 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { NotificationViewModel } from '../view-models/notification-view-model';
+import { Patch } from '@nestjs/common';
+import { Param } from '@nestjs/common';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({ 
+      notificationId: id,
+    }); 
+  }
+
+  async countFromRecipient() {}
+
+  async getFromRecipient() {}
+
+  async read() {}
+
+  async unread() {}
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
