ec4ca0b7f650e5c3668cf8898e0d024e34422b90
Carlos <carlos.msv@hotmail.com>
2022-12-17 00:33:26 -0300

    send-notification injectable so it can be used on notifications.controller; dabase.module using NotificationsRepository provider and exporting NotificationsRepository; http.module imports DatabaseModule and using SendNotification as provider

--- src/infra/http/htttp.module.ts
+++ src/infra/http/htttp.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
-  imports: [],
-  controllers: [
-    NotificationsController,
-  ],
-})
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [SendNotification]})
 export class HttpModule {}
