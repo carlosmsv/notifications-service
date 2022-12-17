1b5f37141856949b93228b376c3881671fb0ac38
Carlos <carlos.msv@hotmail.com>
2022-12-17 03:11:42 -0300

    cancel notification

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,8 +4,15 @@ import { NotificationsRepository } from "@application/repositories/notifications
 
 export class InMemoryNotificationsRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error("Method not implemented.");
+  }
   
   async create(notification: Notification) {
    this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error("Method not implemented.");
   }
 }
