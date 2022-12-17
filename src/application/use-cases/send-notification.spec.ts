42b12af31dafc8176ddc3a5db747ce35ab297cd2
Carlos <carlos.msv@hotmail.com>
2022-12-16 23:38:33 -0300

    notifications repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,17 @@
+import { Notification } from "../entities/notification";
 import { SendNotification } from "./send-notification";
+
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    console.log(notification);
+  }
+}
+
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'This is a test notification',
--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,5 +1,6 @@
 import { Content } from "../entities/content";
 import { Notification } from "../entities/notification";
+import { NotificationsRepository } from "../repositories/notifications-repository";
 
 interface SendNotificationRequest {
   recipientId: string;
@@ -12,6 +13,8 @@ interface SendNotificationResponse {
 }
 
 export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
   async execute(request: SendNotificationRequest): Promise<SendNotificationResponse> {
     const { recipientId, content, category } = request;
 
@@ -20,6 +23,8 @@ export class SendNotification {
       content: new Content(content),
       category,
     });
+
+    await this.notificationsRepository.create(notification);
 
     return {
       notification,
